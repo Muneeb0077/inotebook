e6f25bd9ca7b66a762e9cb2f2399508175c447f5
Muneeb0077 <muneebasif4213@gmail.com>
2023-07-30 15:36:24 +0500

    created routes and schemas

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURi='mongodb://localhost:27017/'
+const mongoURi='mongodb://127.0.0.1:27017/'
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURi,{
--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,11 @@ connextToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Muneeb!')
-})
+//Available Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
