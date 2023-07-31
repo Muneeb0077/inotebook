7a2e2db21a974c328c53ac47cbaa07d56d8b034e
Muneeb0077 <muneebasif4213@gmail.com>
2023-07-31 14:51:05 +0500

    connected with mongodb models

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,17 @@
 const express=require('express');
 const router = express.Router();
+const User=require('../models/User');
 
-router.get('/',(req,res)=>{
-     obj={
-        a:'thios',
-        number:34
-     }
-     res.json(obj)
+
+
+
+// create a User using: POST "/api/auth/". Dosent require auth
+router.post('/',(req,res)=>{
+    
+   console.log(req.body);
+   const user=User(req.body);
+   user.save();
+   res.send(req.body);
 })
 
 module.exports=router
